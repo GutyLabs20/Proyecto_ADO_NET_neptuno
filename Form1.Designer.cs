66dcdeafdb661de8922893452a01f3e32e4e7f87
GutyLabs20 <gutylabs20@gmail.com>
2020-07-25 13:59:06 -0500

    Aprendiendo: Proyecto C# y SQL

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Proyecto_ADO_NET_neptuno
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtcodigo = new System.Windows.Forms.TextBox();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txtprecio = new System.Windows.Forms.TextBox();
+            this.txtstock = new System.Windows.Forms.TextBox();
+            this.cbocategoria = new System.Windows.Forms.ComboBox();
+            this.dateFecha = new System.Windows.Forms.DateTimePicker();
+            this.btnnuevo = new System.Windows.Forms.Button();
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.grilla = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grilla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(90, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(97, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Precio:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(99, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Stock:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(82, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Categoria:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(72, 189);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Fecha Prod:";
+            // 
+            // txtcodigo
+            // 
+            this.txtcodigo.Location = new System.Drawing.Point(156, 23);
+            this.txtcodigo.Name = "txtcodigo";
+            this.txtcodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtcodigo.TabIndex = 1;
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(156, 56);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(190, 20);
+            this.txtnombre.TabIndex = 1;
+            // 
+            // txtprecio
+            // 
+            this.txtprecio.Location = new System.Drawing.Point(156, 88);
+            this.txtprecio.Name = "txtprecio";
+            this.txtprecio.Size = new System.Drawing.Size(100, 20);
+            this.txtprecio.TabIndex = 1;
+            // 
+            // txtstock
+            // 
+            this.txtstock.Location = new System.Drawing.Point(156, 120);
+            this.txtstock.Name = "txtstock";
+            this.txtstock.Size = new System.Drawing.Size(100, 20);
+            this.txtstock.TabIndex = 1;
+            // 
+            // cbocategoria
+            // 
+            this.cbocategoria.FormattingEnabled = true;
+            this.cbocategoria.Location = new System.Drawing.Point(156, 153);
+            this.cbocategoria.Name = "cbocategoria";
+            this.cbocategoria.Size = new System.Drawing.Size(190, 21);
+            this.cbocategoria.TabIndex = 2;
+            // 
+            // dateFecha
+            // 
+            this.dateFecha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateFecha.Location = new System.Drawing.Point(156, 182);
+            this.dateFecha.Name = "dateFecha";
+            this.dateFecha.Size = new System.Drawing.Size(100, 20);
+            this.dateFecha.TabIndex = 3;
+            // 
+            // btnnuevo
+            // 
+            this.btnnuevo.Location = new System.Drawing.Point(30, 220);
+            this.btnnuevo.Name = "btnnuevo";
+            this.btnnuevo.Size = new System.Drawing.Size(110, 33);
+            this.btnnuevo.TabIndex = 4;
+            this.btnnuevo.Text = "Nuevo";
+            this.btnnuevo.UseVisualStyleBackColor = true;
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Location = new System.Drawing.Point(156, 220);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(110, 33);
+            this.btnGrabar.TabIndex = 4;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.btnGrabar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(282, 220);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(110, 33);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // grilla
+            // 
+            this.grilla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grilla.Location = new System.Drawing.Point(8, 265);
+            this.grilla.Name = "grilla";
+            this.grilla.Size = new System.Drawing.Size(426, 177);
+            this.grilla.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 489);
+            this.Controls.Add(this.grilla);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnGrabar);
+            this.Controls.Add(this.btnnuevo);
+            this.Controls.Add(this.dateFecha);
+            this.Controls.Add(this.cbocategoria);
+            this.Controls.Add(this.txtstock);
+            this.Controls.Add(this.txtprecio);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.txtcodigo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grilla)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtcodigo;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.TextBox txtprecio;
+        private System.Windows.Forms.TextBox txtstock;
+        private System.Windows.Forms.ComboBox cbocategoria;
+        private System.Windows.Forms.DateTimePicker dateFecha;
+        private System.Windows.Forms.Button btnnuevo;
+        private System.Windows.Forms.Button btnGrabar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.DataGridView grilla;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data; //Contiene propiedades comunes de ADO, .NET, DataSet, DataTable, etc.
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;    // permite usar SQLConnection, SQLCommand, etc.
+using System.Configuration; //permitir leer el app.config
+
+namespace Proyecto_ADO_NET_neptuno
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Recuperar la cadena de conexion desde el app.config
+        String cad_cn = ConfigurationManager.ConnectionStrings["cn1"].ConnectionString;
+
+        //Traer las categorias
+        void Traer_Categorias()
+        {
+            SqlDataAdapter adapCat = new SqlDataAdapter("Listar_Categorias", cad_cn);
+            //
+            adapCat.SelectCommand.CommandType = CommandType.StoredProcedure;
+            //
+            DataTable dtabla = new DataTable();
+            adapCat.Fill(dtabla);
+            adapCat.Dispose();
+
+            //Enlazar el datatable como origen de datos del combobox
+            cbocategoria.DataSource = dtabla;
+            //indicar la columna del datatable que mostrara sus datos
+            cbocategoria.DisplayMember = "NombreCategoria";
+            //Indica la columna que almacenara el combobox
+        }
+
+        //Listar Productos
+        void Traer_Productos()
+        {
+            SqlDataAdapter adapProd = new SqlDataAdapter("Listar_Producto", cad_cn);
+            //
+            adapProd.SelectCommand.CommandType = CommandType.StoredProcedure;
+            //
+            adapProd.SelectCommand.Parameters.AddWithValue("@Eliminado", "NO");
+            //
+            DataTable dtabla = new DataTable();
+            adapProd.Fill(dtabla);
+            adapProd.Dispose();
+
+            grilla.DataSource = dtabla;
+            grilla.AutoResizeColumns();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Traer_Categorias();
+
+            Traer_Productos();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int xcodigo = int.Parse(txtcodigo.Text);
+                SqlConnection cnx = new SqlConnection(cad_cn);
+                cnx.Open();
+
+                SqlCommand cmd = new SqlCommand("Eliminar_Producto", cnx);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@codprod", xcodigo);
+                cmd.ExecuteNonQuery(); //Ejecuta instrucciones que no sea un SELECT
+
+                cnx.Close();
+
+                MessageBox.Show("Producto Eliminado");
+
+                Traer_Productos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //para recuperar el valor almacenado por la columna indicada en la propiedad
+                // ValueMember de un combobox o listbox, realizamos lo siguiente:
+                int xcodcat = cbocategoria.SelectedIndex;
+
+                SqlConnection cnx = new SqlConnection(cad_cn);
+                cnx.Open();
+
+                SqlCommand cmd = new SqlCommand("Grabar_Producto", cnx);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@nomprod", txtnombre.Text);
+                cmd.Parameters.AddWithValue("@precio", decimal.Parse(txtprecio.Text));
+                cmd.Parameters.AddWithValue("@stock", short.Parse(txtstock.Text));
+                cmd.Parameters.AddWithValue("@codcat", xcodcat);
+                cmd.Parameters.AddWithValue("@fechaprod", dateFecha.Value);
+
+                txtcodigo.Text = cmd.ExecuteScalar().ToString();//Ejecuta una linea select
+
+                cnx.Close();
+
+                MessageBox.Show("Nuevo producto grabado correctamente");
+
+                Traer_Productos();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
